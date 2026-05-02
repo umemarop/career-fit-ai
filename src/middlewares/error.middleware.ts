c00b10b1fecfdf7a68212a45c7ceab84d6f2b780
Sanghun Han <umemarop@gmail.com>
2026-05-02 13:02:26 +1000

    feat: set up global error handling system

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,21 @@
+import type { Request, Response, NextFunction } from "express";
+
+type ErrorWithStatus = Error & {
+  statusCode?: number;
+  status?: string;
+};
+
+export const errorController = (
+  err: ErrorWithStatus,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const statusCode = err.statusCode || 500;
+  const status = err.status || "error";
+
+  res.status(statusCode).json({
+    status,
+    message: err.message || "Something went wrong",
+  });
+};
